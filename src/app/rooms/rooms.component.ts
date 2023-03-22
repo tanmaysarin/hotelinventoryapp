888f53974c9d8079a6d87bcb84f1898814603285
Tanmay Sarin <tanmaysarin@gmail.com>
2023-03-21 23:14:45 -0500

    created tables and added bootstrap

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Room, RoomList } from './rooms';
 
 @Component({
   selector: 'hinv-rooms',
@@ -9,10 +10,36 @@ export class RoomsComponent {
 
   hotelName = 'Hilton Hotel';
   numberOfRooms = 10;
-
   hideRooms = false;
 
-  toggle(){
+  rooms: Room = {
+    totalRooms: 20,
+    availableRooms: 10,
+    bookedRooms: 5,
+  }
+
+  roomList: RoomList[] = [
+    {
+      roomNumber: 1,
+      roomType: 'Deluxe Room',
+      amenities: 'Air Conditioner',
+      price: 500,
+      photos: 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg',
+      checkinTime: new Date('21-Mar-2023'),
+      checkoutTime: new Date('22-Mar-2023'),
+    },
+    {
+      roomNumber: 2,
+      roomType: 'Private Suite',
+      amenities: 'Air Conditioner, TV, Wifi',
+      price: 1000,
+      photos: 'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg',
+      checkinTime: new Date('23-Mar-2023'),
+      checkoutTime: new Date('24-Mar-2023'),
+    }
+]
+
+  toggle() {
     this.hideRooms = !this.hideRooms;
   }
 
